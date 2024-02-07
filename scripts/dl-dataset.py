c2fc9487986c90fbe6f646183c4fee1722d11e63
Erouhh <eero.kuosmanen26@gmail.com>
2024-02-07 16:22:13 +0200

    add dataset link to comment

--- scripts/dl-dataset.py
+++ scripts/dl-dataset.py
@@ -1,1 +1,2 @@
 # Script for downloading the dataset from the web, and extracting it to the data folder
+# https://www.kaggle.com/datasets/lokeshparab/amazon-products-dataset/download?datasetVersionNumber=2
